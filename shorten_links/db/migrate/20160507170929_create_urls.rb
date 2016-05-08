f8ab6c949fabbe267bb72ee28f6bcc7af4b51395
Mark Novak <mnovak@mit.edu>
2016-05-08 09:31:58 -0400

    add visits to migration ..do not add it directly to sqlite

--- shorten_links/db/migrate/20160507170929_create_urls.rb
+++ shorten_links/db/migrate/20160507170929_create_urls.rb
@@ -3,7 +3,7 @@ class CreateUrls < ActiveRecord::Migration
     create_table :urls do |t|
       t.text :full_url
       t.string :short_url
-
+      t.integer :visits
       t.timestamps null: false
     end
   end
